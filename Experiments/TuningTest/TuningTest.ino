59f0f5f90ef118db10e9ee7a0956c0b70b8f85f8
Runze Yuan <1959180242@qq.com>
2022-11-20 19:14:22 +0000

    11_20实验工具

--- Experiments/TuningTest/TuningTest.ino
+++ Experiments/TuningTest/TuningTest.ino
@@ -21,10 +21,13 @@ StepSignal step;
 Evaluator eva;
 
 volatile int delay_value = 300;
-volatile unsigned long prev_millis = 0;
+volatile unsigned long prev_micros = 0;
+volatile bool printed = false;
+volatile int count = 0;
+double ove[11];
+double set[11];
 
-void setup()
-{
+void setup() {
   Serial.begin(9600);
   delay(1);
   Serial.print("***RESET***");
@@ -37,42 +40,84 @@ void setup()
   // 对于300的delay，此三值分别为30 0.11 -950 或:35 0.1 950
   // 对于500的delay，此三值分别为35 0.15 500
   double Kp = 35;
-  double Ki = 2.5;
-  double Kd = 5;
-  delay_value = 10;
+  double Ki = 0.05;
+  double Kd = 950;
+  delay_value = 500;
   step.init(5000, 1.0);
 
   unsigned long PID_update_cycle_length = 0;
   double I_limit = 5000;
-  pid_lspeed.init(Kp, Ki, Kd, I_limit, PID_update_cycle_length); // P,I,D,积分限幅值（暂时没用，只是占位符），PID更新时间间隔（单位毫秒），系统本身能做到的最快的速度也就是500微秒更新一次
+  pid_lspeed.init(Kp, Ki, Kd, I_limit, PID_update_cycle_length);  // P,I,D,积分限幅值（暂时没用，只是占位符），PID更新时间间隔（单位毫秒），系统本身能做到的最快的速度也就是500微秒更新一次
 }
 
-void loop()
-{
-  if (prev_millis == 0)
-  {
-    prev_millis = millis();
+void loop() {
+  if (prev_micros == 0) {
+    prev_micros = millis();
   }
 
-  if ((millis() - prev_millis) >= delay_value)
-  {  
+  if ((millis() - prev_micros) >= delay_value) {
     ki.update();
     step.update();
     // 进行pid相关的操作
-    if (step.value == step.high) // 阶跃函数为高时，用pid控制电机速度，开评估器，评估pid性能
+    if (step.value == step.high)  // 阶跃函数为高时，用pid控制电机速度，开评估器，评估pid性能
     {
-      pid_lspeed.aim = step.value;            // 将阶跃信号的值设为速度控制PID的追踪对象
-      pid_lspeed.cal(ki.v_l);                 // 根据当前速度计算PID控制值
-      motor.setPower(pid_lspeed.result, 0);   // 用PID结果控制电机
-      eva.record(ki.v_l, pid_lspeed.aim, on); // 评估pid性能
-    }
-    else // 阶跃函数为低时：
-    {    // 重置pid，关电机，关pid评估器
+      pid_lspeed.aim = step.value;             // 将阶跃信号的值设为速度控制PID的追踪对象
+      pid_lspeed.cal(ki.v_l);                  // 根据当前速度计算PID控制值
+      motor.setPower(pid_lspeed.result, 0);    // 用PID结果控制电机
+      eva.record(ki.v_l, pid_lspeed.aim, on);  // 评估pid性能
+
+      if (eva.ended) {
+        if ((printed == false) && (count <= 11)) {
+          Serial.println("=====================");
+          Serial.print("sigma:");
+          Serial.println(eva.overshoot);
+          Serial.print("ts:");
+          Serial.println(eva.setting);
+          printed = true;
+          Serial.println("↑");
+          Serial.print(count);
+          Serial.print(count);
+          Serial.print(count);
+          Serial.print(count);
+          Serial.print(count);
+          Serial.println(count);
+
+          ove[count] = eva.overshoot;
+          set[count] = eva.setting;
+          if (count == 10) {
+
+            Serial.println("");
+            Serial.println("overshoot:");
+            // 输出格式处理好的实验数据
+            for (int i = 1; i < 11; i++) {
+              Serial.print("|");
+              if (ove[i] != INVALID_PARA) {
+
+                Serial.print(ove[i] * 100.0);
+
+              } else {
+                Serial.print(0);
+              }
+            }
+            Serial.println("|");
+            Serial.println("setting time:");
+            for (int i = 1; i < 11; i++) {
+              Serial.print("|");
+              Serial.print(set[i]);
+            }
+            Serial.println("|");
+          }
+          count += 1;
+        }
+      }
+    } else  // 阶跃函数为低时：
+    {       // 重置pid，关电机，关pid评估器
       pid_lspeed.reset();
       motor.setPower(0, 0);
       eva.reset();
+      printed = false;
     }
-
+    /*
     Serial.print("p:");
     Serial.println(pid_lspeed.P);
     Serial.print("1:");
@@ -83,15 +128,14 @@ void loop()
     Serial.println(ki.v_l);
     Serial.print("0:");
     Serial.println(0);
-    // Serial.print("sigma:");
-    // Serial.println(eva.overshoot);
-    // Serial.print("ts:");
-    // Serial.println(eva.setting);
+    Serial.print("sigma:");
+    Serial.println(eva.overshoot);
+    Serial.print("ts:");
+    Serial.println(eva.setting);
+*/
     // 更新时间记录
-    prev_millis = millis();
-  }
-  else
-  {
+    prev_micros = millis();
+  } else {
     // 不然跳过
   }
 }
