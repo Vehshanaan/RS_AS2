2abff055169046ecefed77c88a9175e56ad54e9b
Runze Yuan <1959180242@qq.com>
2022-12-01 17:58:19 +0000

    轮子速度与PWM值的变化关系

--- Report/ReportCodes/WheelResponse/result.py
+++ Report/ReportCodes/WheelResponse/result.py
@@ -261,7 +261,7 @@ import matplotlib.pyplot as plt
 fig = plt.figure
 plt.plot(range(256),speed)
 plt.xlim = ([-5,230])
-plt.xlabel("PWM duty cycle ratio (0~255 for 0%~100%)")
-plt.ylabel("Left wheel speed calculated by kinematics (m/s)")
+plt.xlabel("PWM duty cycle ratio (0~255 for 0%~100%)",fontsize = 13)
+plt.ylabel("Left wheel speed calculated by kinematics (m/s)",fontsize=13)
 plt.grid()
 plt.show()
